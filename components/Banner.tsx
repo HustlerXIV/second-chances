583374116b384f65c8dc8333506fcc1886e4f5ab
TangNapat <napat.jaiyim@hotmail.com>
2024-10-16 23:15:32 +0700

    [UPDATE] Utilzied Splide

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Container from "./Container";
 import CustomButton from "./CustomButton";
 import { useRouter } from "next/navigation";
+import "@splidejs/react-splide/css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 
 const title = "EVERY PET<br/> DESERVES A<br/>HOME";
 const subtitle =
@@ -16,46 +18,80 @@ const Banner = () => {
 
   return (
     <div
-      className="relative w-full h-[600px] bg-cover bg-center"
+      className="relative w-full h-[522px] bg-cover bg-center"
       style={{ backgroundImage: "url('/images/banner-bg.svg')" }}
     >
-      <Container
-        style={{
-          height: "100%",
-          display: "flex",
-          alignItems: "flex-end",
-          justifyContent: "center",
-          gap: "40px",
+      <Splide
+        options={{
+          type: "loop",
+          autoplay: true,
+          pagination: false,
+          arrows: false,
         }}
       >
-        <div className="h-full flex flex-col justify-center gap-[20px]">
-          <div
-            className="text-black font-bold"
-            style={{ fontSize: "64px", lineHeight: "80px" }}
-            dangerouslySetInnerHTML={{ __html: title }}
-          ></div>
-          <div
-            style={{ fontSize: "20px" }}
-            className="text-customDarkGray font-bold"
-            dangerouslySetInnerHTML={{ __html: subtitle }}
-          ></div>
-          <CustomButton
-            label="ADOPT NOW"
-            variant="contained"
-            sx={{ maxWidth: "200px", fontWeight: "bold" }}
-            onClick={() => handleOnClick()}
-          />
-        </div>
-        <div className="max-w-[580px]">
-          <Image
-            src="/images/dog-and-kid.svg"
-            layout="responsive"
-            alt=""
-            width={500}
-            height={300}
-          />
-        </div>
-      </Container>
+        <SplideSlide>
+          <Container>
+            <div className="flex h-full pt-[20px] flex-col items-center justify-center gap-[40px] md:flex-row md:items-center md:pt-0">
+              <div className="h-full flex flex-col justify-center gap-[20px]">
+                <div
+                  className="text-black font-bold text-[32px] leading-[40px] md:text-[64px] md:leading-[80px]"
+                  dangerouslySetInnerHTML={{ __html: title }}
+                ></div>
+                <div
+                  className="text-customDarkGray font-bold text-[20px]"
+                  dangerouslySetInnerHTML={{ __html: subtitle }}
+                ></div>
+                <CustomButton
+                  label="ADOPT NOW"
+                  variant="contained"
+                  sx={{ maxWidth: "200px", fontWeight: "bold" }}
+                  onClick={() => handleOnClick()}
+                />
+              </div>
+              <div className="w-full max-w-[200px] md:max-w-[580px]">
+                <Image
+                  src="/images/dog-and-kid.svg"
+                  layout="responsive"
+                  alt=""
+                  width={0}
+                  height={0}
+                />
+              </div>
+            </div>
+          </Container>
+        </SplideSlide>
+        <SplideSlide>
+          <Container>
+            <div className="flex h-full pt-[20px] flex-col items-center justify-center gap-[40px] md:flex-row md:items-center md:pt-0">
+              <div className="w-full max-w-[200px] md:max-w-[580px]">
+                <Image
+                  src="/images/dog-and-kid.svg"
+                  layout="responsive"
+                  alt=""
+                  width={0}
+                  height={0}
+                />
+              </div>
+              <div className="h-full flex flex-col justify-center gap-[20px]">
+                <div
+                  className="text-black font-bold text-[32px] leading-[40px] md:text-[64px] md:leading-[80px]"
+                  dangerouslySetInnerHTML={{ __html: "JOIN US!" }}
+                ></div>
+                <div
+                  className="text-customDarkGray font-bold text-[20px]"
+                  dangerouslySetInnerHTML={{ __html: "Do the good things" }}
+                ></div>
+                <CustomButton
+                  label="JOIN US"
+                  design="containedBlack"
+                  sx={{ maxWidth: "200px", fontWeight: "bold" }}
+                  onClick={() => router.push("/sign-up")}
+                />
+              </div>
+            </div>
+          </Container>
+        </SplideSlide>
+      </Splide>
     </div>
   );
 };
